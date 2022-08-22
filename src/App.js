6f2ae71f46a17941d00f87b7021d263954020376
mohammadhossein211 <mohammadhossein211@gmail.com>
2022-08-22 15:20:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [config, setConfig] = useState({
+        source: {
+            defaults: {
+                service: 'bintu',
+            },
+            entries: [
+                {
+                    h5live: {
+                        rtmp: {
+                            streamname: '9yj0J-PKZRQ',
+                        },
+                    },
+                },
+            ],
+        },
+        playback: {
+            autoplay: true,
+            automute: true,
+            muted: false,
+        },
+        style: {
+            width: '100%',
+            height: '100%',
+            aspectratio: '16/9',
+            controls: true,
+            backgroundColor: 'gray',
+            symbolColor: 'red',
+            controlBarColor: 'blue',
+            buttonCursor: 'pointer',
+            // poster: 'https://www.nanocosmos.de/images/BroadcastH5Live-Schema4.png',
+        },
+        events: {
+            // onReady: () => {
+            //     alert('ready!')
+            // },
+            // onPlay: () => {
+            //     alert('onPlay')
+            // },
+        },
+    })
+
+    function setupNanoplayer(config) {
+        var nanoPlayer = new window.NanoPlayer('playerDiv')
+
+        nanoPlayer.setup(config).then(
+            function (config) {
+                console.log('setup success')
+                console.log('config: ' + JSON.stringify(config, undefined, 4))
+            },
+            function (error) {
+                alert(error.message)
+            }
+        )
+    }
+
+    useEffect(() => {
+        setupNanoplayer(config)
+    }, [])
+
+    return (
+        <div className='App'>
+            <h1>Hossein's Stream</h1>
+            <div id='playerDiv' />
+        </div>
+    )
 }
 
-export default App;
+export default App
